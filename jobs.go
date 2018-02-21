36b3b44504a85c85d6d9deb7a9e8a29d6b99a598
Michel Vocks <michel.vocks@dbschenker.com>
2018-02-21 22:01:02 +0100

    Restructured test pipeline

--- jobs.go
+++ jobs.go
@@ -1,65 +1,50 @@
 package main
 
 import (
-	"github.com/gaia-pipeline/gaia/proto"
 	sdk "github.com/gaia-pipeline/gosdk"
 )
 
-var jobs = []sdk.JobsWrapper{
-	sdk.JobsWrapper{
-		FuncPointer: CreateUser,
-		Job: proto.Job{
-			Title:       "Create DB User",
-			Description: "Creates a database user with least privileged permissions.",
-			Priority:    0,
-		},
+var jobs = sdk.Jobs{
+	sdk.Job{
+		Handler:     CreateUser,
+		Title:       "Create DB User",
+		Description: "Creates a database user with least privileged permissions.",
+		Priority:    0,
 	},
-	sdk.JobsWrapper{
-		FuncPointer: MigrateDB,
-		Job: proto.Job{
-			Title:       "DB Migration",
-			Description: "Imports newest test data dump and migrates to newest version.",
-			Priority:    10,
-		},
+	sdk.Job{
+		Handler:     MigrateDB,
+		Title:       "DB Migration",
+		Description: "Imports newest test data dump and migrates to newest version.",
+		Priority:    10,
 	},
-	sdk.JobsWrapper{
-		FuncPointer: CreateNamespace,
-		Job: proto.Job{
-			Title:       "Create K8S Namespace",
-			Description: "Creates a new Kubernetes namespace for the new test environment.",
-			Priority:    20,
-		},
+	sdk.Job{
+		Handler:     CreateNamespace,
+		Title:       "Create K8S Namespace",
+		Description: "Creates a new Kubernetes namespace for the new test environment.",
+		Priority:    20,
 	},
-	sdk.JobsWrapper{
-		FuncPointer: CreateDeployment,
-		Job: proto.Job{
-			Title:       "Create K8S Deployment",
-			Description: "Creates a new Kubernetes deployment for the new test environment.",
-			Priority:    30,
-		},
+	sdk.Job{
+		Handler:     CreateDeployment,
+		Title:       "Create K8S Deployment",
+		Description: "Creates a new Kubernetes deployment for the new test environment.",
+		Priority:    30,
 	},
-	sdk.JobsWrapper{
-		FuncPointer: CreateService,
-		Job: proto.Job{
-			Title:       "Create K8S Service",
-			Description: "Creates a new Kubernetes service for the new test environment.",
-			Priority:    30,
-		},
+	sdk.Job{
+		Handler:     CreateService,
+		Title:       "Create K8S Service",
+		Description: "Creates a new Kubernetes service for the new test environment.",
+		Priority:    30,
 	},
-	sdk.JobsWrapper{
-		FuncPointer: CreateIngress,
-		Job: proto.Job{
-			Title:       "Create K8S Ingress",
-			Description: "Creates a new Kubernetes ingress for the new test environment.",
-			Priority:    30,
-		},
+	sdk.Job{
+		Handler:     CreateIngress,
+		Title:       "Create K8S Ingress",
+		Description: "Creates a new Kubernetes ingress for the new test environment.",
+		Priority:    30,
 	},
-	sdk.JobsWrapper{
-		FuncPointer: Cleanup,
-		Job: proto.Job{
-			Title:       "Clean up",
-			Description: "Removes all temporary files.",
-			Priority:    40,
-		},
+	sdk.Job{
+		Handler:     Cleanup,
+		Title:       "Clean up",
+		Description: "Removes all temporary files.",
+		Priority:    40,
 	},
 }
--- main.go
+++ main.go
@@ -8,69 +8,85 @@ import (
 )
 
 func CreateUser() error {
-	log.Println("CreateUser has been executed!")
+	log.Println("CreateUser has been started!")
 
 	// lets sleep to simulate that we do something
 	time.Sleep(15 * time.Second)
+
+	log.Println("CreateUser has been finished!")
 
 	return nil
 }
 
 func MigrateDB() error {
-	log.Println("MigrateDB has been executed!")
+	log.Println("MigrateDB has been started!")
 
 	// lets sleep to simulate that we do something
 	time.Sleep(15 * time.Second)
+
+	log.Println("MigrateDB has been finished!")
 
 	return nil
 }
 
 func CreateNamespace() error {
-	log.Println("CreateNamespace has been executed!")
+	log.Println("CreateNamespace has been started!")
 
 	// lets sleep to simulate that we do something
 	time.Sleep(15 * time.Second)
+
+	log.Println("CreateNamespace has been finished!")
 
 	return nil
 }
 
 func CreateDeployment() error {
-	log.Println("CreateDeployment has been executed!")
+	log.Println("CreateDeployment has been started!")
 
 	// lets sleep to simulate that we do something
 	time.Sleep(15 * time.Second)
+
+	log.Println("CreateDeployment has been finished!")
 
 	return nil
 }
 
 func CreateService() error {
-	log.Println("CreateService has been executed!")
+	log.Println("CreateService has been started!")
 
 	// lets sleep to simulate that we do something
 	time.Sleep(15 * time.Second)
+
+	log.Println("CreateService has been finished!")
 
 	return nil
 }
 
 func CreateIngress() error {
-	log.Println("CreateIngress has been executed!")
+	log.Println("CreateIngress has been started!")
 
 	// lets sleep to simulate that we do something
 	time.Sleep(15 * time.Second)
+
+	log.Println("CreateIngress has been finished!")
 
 	return nil
 }
 
 func Cleanup() error {
-	log.Println("Cleanup has been executed!")
+	log.Println("Cleanup has been started!")
 
 	// lets sleep to simulate that we do something
 	time.Sleep(15 * time.Second)
+
+	log.Println("Cleanup has been finished!")
 
 	return nil
 }
 
 func main() {
 	// Serve
-	sdk.Serve(jobs)
+	if err := sdk.Serve(jobs); err != nil {
+		panic(err)
+	}
 }
